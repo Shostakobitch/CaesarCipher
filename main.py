9b806f439de3b582cb005964fe357ab08ff29964
V V <82732579+Shostakobitch@users.noreply.github.com>
2022-06-11 06:23:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(direction, text, shift):
+  
+  encryption_number = [] 
+  
+  for characters in text:
+       my_int = alphabet.index(characters)
+       encryption_number.append(my_int)
+    
+  encryption_number_shifted = [] 
+  
+  for numbers in encryption_number:
+       shifted_number = numbers + shift
+       encryption_number_shifted.append(shifted_number)
+  print(encryption_number_shifted)
+
+  shifted_characters = []
+
+  for numbers in encryption_number_shifted:
+      shifted_characters += alphabet[numbers]
+  print(shifted_characters)
+
+  encrypted_string =''.join(shifted_characters)
+  print(encrypted_string)
+encrypt(direction, text, shift)
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
